a7f1f2abc8ed8aaa9ddd7a52614d4d2be656dc1d
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-04-03 11:39:57 -0500

    first commit

---
+++ src/main/java/com/example/bibliotecaSpringBootMongoDB/BibliotecaSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecaSpringBootMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bibliotecaSpringBootMongoDB/BibliotecaSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecaSpringBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
