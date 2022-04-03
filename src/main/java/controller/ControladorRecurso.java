10e84fc3980425c75248069dbf84c7e3040b05ff
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-04-03 12:56:11 -0500

    se crear el enpoint para consultar la disponibilidad del recurso

--- src/main/java/controller/ControladorRecurso.java
+++ src/main/java/controller/ControladorRecurso.java
@@ -4,10 +4,7 @@ import dto.RecursoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import servicio.ServicioRecurso;
 
 @RestController
@@ -21,5 +18,11 @@ public class ControladorRecurso {
     public ResponseEntity<RecursoDTO> create(@RequestBody RecursoDTO recursoDTO) {
         return new ResponseEntity(servicioRecurso.crear(recursoDTO), HttpStatus.CREATED);
     }
-    
+
+    @GetMapping("/disponibilidad/{id}")
+    public ResponseEntity<RecursoDTO> mostrarDisponibilidad(@PathVariable("id") String id) {
+        return new ResponseEntity(servicioRecurso.comprobarDisponibilidad(id), HttpStatus.OK);
+    }
+
+
 }
